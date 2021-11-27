46d94e105484129256a5cb5c1edb66e05e116a70
sjmoser <sarahjeanemoser@gmail.com>
2021-11-27 13:55:35 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+import Weather from "./Weather.js";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather Search Engine</h1>
+      <Weather />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
